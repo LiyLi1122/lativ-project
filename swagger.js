ee87c276a2d574cf7109193252f47240c91feffb
LiyLi1122 <teppei.252033@gmail.com>
2023-01-22 19:33:55 +0800

    feat: add swagger-autogen doc

--- swagger.js
+++ swagger.js
@@ -1,7 +1,16 @@
+
 const swaggerAutogen = require('swagger-autogen')()
+const doc = {
+  info: {
+    title: 'Lativ-Project apis'
+  },
+  host: 'lativ-database.c9y6fmfie00t.ap-northeast-1.rds.amazonaws.com:3000',
+  schemes: ['http', 'https']
+}
+
 // 輸出檔案名稱
 const outputFile = './swagger_output.json'
 // 指向 API 根文件位置(規定格式要用 Array)
 const endpointFiles = ['./app.js']
 
-swaggerAutogen(outputFile, endpointFiles)
+swaggerAutogen(outputFile, endpointFiles, doc)
